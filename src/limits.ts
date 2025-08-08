240560e767eed74fb4eefe26d99ba7360f00bd1b
Kay Wallaschek <kayluis.wallaschek@gmail.com>
2025-08-08 22:01:15 +0900

    Test commit

--- src/limits.ts
+++ src/limits.ts
@@ -8,20 +8,20 @@ export class TokenLimits {
     this.knowledgeCutOff = '2021-09-01'
     if (model === 'gpt-4-32k') {
       this.maxTokens = 32600
-      this.responseTokens = 4000
-    } else if (model === 'gpt-3.5-turbo-16k') {
+      this.responseTokens = 40
+    } else if (model === 'gpt-3o-16k') {
       this.maxTokens = 16300
       this.responseTokens = 3000
     } else if (model === 'gpt-4') {
       this.maxTokens = 8000
       this.responseTokens = 2000
     } else if (model === 'gpt-4o') {
-      this.maxTokens = 128000;
-      this.responseTokens = 4096;
+      this.maxTokens = 128000
+      this.responseTokens = 4096
     } else if (model === 'gpt-5') {
-      this.maxTokens = 200000;
-      this.responseTokens = 8192;
-      this.knowledgeCutOff = '2024-04-01';
+      this.maxTokens = 200000
+      this.responseTokens = 8192
+      this.knowledgeCutOff = '2024-04-01'
     } else {
       this.maxTokens = 4000
       this.responseTokens = 1000
